fd462bb9dca123014eb6018e9959d6ea4e07c2c7
yuk1ram <liyaxxxkard@gmail.com>
2025-11-10 11:14:31 +0500

    Delete lesson10.test9.py

--- lesson10.test9.py
+++
@@ -1,9 +0,0 @@
-number = 384
-sum_digits = 0
-for digit in str(number):
-    sum_digits = sum_digits + int(digit)
-if sum_digits % 2 == 0:
-    result = sum_digits * 2
-else:
-    result = sum_digits + 1
-print(result)
